70d46f7b7f3ac9ee593bd43a61185dddf172444d
sreevidyachowlur <sreevidyachowlur@gmail.com>
2024-05-24 11:40:19 +0530

    module source address modification

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "web" {
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group-aws"
+  source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name    = "blog_sree"
 
