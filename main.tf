b851083ebb5ea0ec68b68eebdccb4680c8a68bdd
sreevidyachowlur <sreevidyachowlur@gmail.com>
2024-05-24 12:52:08 +0530

    blog to web

--- main.tf
+++ main.tf
@@ -18,21 +18,21 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.web_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-module "blog_sg" {
+module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name    = "blog_sree"
+  name    = "web_sree"
 
   vpc_id = data.aws_vpc.default.id
 
@@ -44,39 +44,39 @@ module "blog_sg" {
 
 }
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
+resource "aws_security_group" "web" {
+  name        = "web"
   description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "web_http_in" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.web.id
 }
 
-resource "aws_security_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "web_https_in" {
   type              = "ingress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.web.id
 }
 
-resource "aws_security_group_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "web_everything_out" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.web.id
 }
